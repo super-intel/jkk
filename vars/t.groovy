8dc24c79762951fdfb8de7cfc29401afe00c9062
Super Ni <super.ni@intel.com>
2024-01-17 20:42:49 +0800

    0117c

--- vars/t.groovy
+++ vars/t.groovy
@@ -8,6 +8,30 @@ def t2() {
     echo 't.t2()'
 }
 
+def stg_t3() {
+    stage('t3') {
+        steps {
+            script {
+                echo 'stg_t3'
+            }
+        }
+    }
+}
+
+def stg_bld(name) {
+    return {
+        stage("${name}") {
+            script {
+                echo "${name}"
+            }
+        }
+    }
+}
+
 return [
     t1: this.&t1,
-    t2: this.&t2 ]
+    t2: this.&t2,
+    stg_t3: this.&stg_t3,
+    stg_bld: this.&stg_bld
+    
+]
